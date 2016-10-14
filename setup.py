9d2bd13082287dde035a2e177470712e43c01765
Bryan Harter <harter@lasp.colorado.edu>
2016-10-14 17:31:37 -0600

    Fixing major bug with specplot
    May have sped it up too actually

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
         return f.read()
 
 setup(name='pytplot',
-      version='0.1.2',
+      version='0.1.4',
       description='A python version of IDL tplot libraries',
       url='http://github.com/MAVENSDC/Pytplot',
       author='MAVEN SDC',
