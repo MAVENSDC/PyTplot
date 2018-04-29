c75731a70e6a296437e63594d77f1469b0efef36
Bryan Harter <harter@lasp.colorado.edu>
2018-04-29 17:51:18 -0600

    Forgot to update version number

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
         return f.read()
 
 setup(name='pytplot',
-      version='1.1.10',
+      version='1.1.12',
       description='A python version of IDL tplot libraries',
       url='http://github.com/MAVENSDC/Pytplot',
       author='MAVEN SDC',
