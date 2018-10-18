2d15c757f72912eced46a4e5db3059e5ccda050a
Bryan Harter <harter864@gmail.com>
2018-10-18 17:01:24 -0600

    Trying out testing

--- tests/test_cdf_to_tplot.py
+++ tests/test_cdf_to_tplot.py
@@ -2,8 +2,8 @@ import pytplot
 
 def test_cdf_euv_read():
     pytplot.cdf_to_tplot("/home/travis/build/MAVENSDC/PyTplot/tests/testfiles/mvn_euv_l2_bands_20170619_v09_r03.cdf")
-    pytplot.tplot(0)
+    pytplot.tplot(0, testing=True)
 
 def test_cdf_swe_read():
     pytplot.cdf_to_tplot("/home/travis/build/MAVENSDC/PyTplot/tests/testfiles/mvn_swe_l2_svyspec_20170619_v04_r04.cdf")
-    pytplot.tplot(0)
+    pytplot.tplot(0, testing=True)
