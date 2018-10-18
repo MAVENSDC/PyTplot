b22782d8d95a12cfb5e1f6d460a556dd44bb907d
Bryan Harter <harter864@gmail.com>
2018-10-18 15:44:20 -0600

    Accidentally put wrong version number on the testing files

--- tests/test_cdf_to_tplot.py
+++ tests/test_cdf_to_tplot.py
@@ -2,4 +2,4 @@ import pytplot
 
 def test_cdf_read():
     pytplot.cdf_to_tplot("/home/travis/build/MAVENSDC/PyTplot/tests/testfiles/mvn_euv_l2_bands_20170619_v09_r03.cdf")
-    pytplot.cdf_to_tplot("/home/travis/build/MAVENSDC/PyTplot/tests/testfiles/mvn_swe_l2_svyspec_20170619_v04_r02.cdf")
+    pytplot.cdf_to_tplot("/home/travis/build/MAVENSDC/PyTplot/tests/testfiles/mvn_swe_l2_svyspec_20170619_v04_r04.cdf")
