bce319bdced06bd53edb14b2f463b165dac37c98
Bryan Harter <harter864@gmail.com>
2018-10-18 15:35:00 -0600

    Adding slightly more things to test

--- tests/test_cdf_to_tplot.py
+++ tests/test_cdf_to_tplot.py
@@ -1,4 +1,5 @@
 import pytplot
 
 def test_cdf_read():
-    pass
+    pytplot.cdf_to_tplot("/home/travis/build/MAVENSDC/PyTplot/tests/testfiles/mvn_euv_l2_bands_20170619_v09_r03.cdf")
+    pytplot.cdf_to_tplot("/home/travis/build/MAVENSDC/PyTplot/tests/testfiles/mvn_swe_l2_svyspec_20170619_v04_r02.cdf")
