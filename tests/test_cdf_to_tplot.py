79501bc162ffeb982879076400735f4e10c78611
Bryan Harter <harter864@gmail.com>
2018-10-18 16:43:38 -0600

    Seeing what plotting does to travis

--- tests/test_cdf_to_tplot.py
+++ tests/test_cdf_to_tplot.py
@@ -1,5 +1,9 @@
 import pytplot
 
-def test_cdf_read():
+def test_cdf_euv_read():
     pytplot.cdf_to_tplot("/home/travis/build/MAVENSDC/PyTplot/tests/testfiles/mvn_euv_l2_bands_20170619_v09_r03.cdf")
+    pytplot.tplot(0)
+
+def test_cdf_swe_read():
     pytplot.cdf_to_tplot("/home/travis/build/MAVENSDC/PyTplot/tests/testfiles/mvn_swe_l2_svyspec_20170619_v04_r04.cdf")
+    pytplot.tplot(0)
