873b5c25e46270797cac2d6f0b9c2c7e137af016
Bryan Harter <harter864@gmail.com>
2018-10-19 11:07:35 -0600

    Changing the directory to be generalizable

--- tests/test_cdf_to_tplot.py
+++ tests/test_cdf_to_tplot.py
@@ -1,11 +1,15 @@
 import pytplot
+import os
+
+current_directory = os.path.dirname(os.path.realpath(__file__))
 
 def test_cdf_euv_read():
-    pytplot.cdf_to_tplot("C:/Code Repos/PyTplot/tests/testfiles/mvn_euv_l2_bands_20170619_v09_r03.cdf")
+
+    pytplot.cdf_to_tplot(current_directory + "/testfiles/mvn_euv_l2_bands_20170619_v09_r03.cdf")
     pytplot.tplot(0, testing=True)
 
 def test_cdf_swe_read():
-    pytplot.cdf_to_tplot("C:/Code Repos/PyTplot/tests/testfiles/mvn_swe_l2_svyspec_20170619_v04_r04.cdf")
+    pytplot.cdf_to_tplot(current_directory + "/testfiles/mvn_swe_l2_svyspec_20170619_v04_r04.cdf")
     pytplot.options('diff_en_fluxes', 'colormap', 'magma')
     pytplot.options('diff_en_fluxes', 'ztitle', 'FLUX')
     pytplot.options('diff_en_fluxes', 'ytitle', 'Energy')
