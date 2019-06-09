3e98b56c14bb9b09f16e7cc7d103147eb57ff601
Bryan Harter <harter@lasp.colorado.edu>
2019-06-09 15:23:35 -0600

    Stopping pyspedas unit test for now

---
+++ tests/test_pyspedas.py
@@ -0,0 +1,19 @@
+import pytplot
+import pyspedas
+from pyspedas.mms import mms_load_fpi
+
+def test_pyspedas():
+	pass
+	'''
+    probe = '1'
+    trange = ['2015-12-01/12:05:00', '2015-12-01/12:07:00']
+
+    mms_load_fpi(probe=probe, datatype=['des-moms', 'dis-moms'], trange=trange)  # center_measurements?
+
+    DenIN = 'mms' + probe + '_dis_numberdensity_fast'
+
+    SpecIN = 'mms' + probe + '_dis_energyspectr_omni_fast'
+
+    pytplot.tplot([SpecIN, DenIN], testing=True)
+    pytplot.tplot([SpecIN, DenIN], testing=True, bokeh=True)
+	'''
