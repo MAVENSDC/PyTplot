f8469964d3078962540d73e86bb3889acabcc318
Bryan Harter <harter@lasp.colorado.edu>
2019-09-12 11:08:54 -0600

    Converting the Coffeescript to TypeScript

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
+#Uploading new versions to pypi:
 #
-#To upload the latest version, change "version=0.X.X+1" and type:
-#	python setup.py sdist
-#   twine upload dist/*
-#
-#
+#python setup.py sdist
+#twine upload dist/*
+#Username MAVENSDC
 
-from setuptools import setup
+
+
+#Uploading new versions to conda:
+#
+#conda-build pytplot
+#conda-build --python 3.6 pytplot
+#conda-build --python 3.5 pytplot
+#conda convert -f --platform all /path/to/created/bundles/file.tar.bz2 -o /path/to/place/converted/files
+#anaconda upload /path/to/created/or/converted/bundles/file.tar.bz2
+#Username MAVENSDC
 
 from setuptools import setup
 setup()
