0c7d4c008f3c658b6c28a8bb13ae1a3622383511
Bryan Harter <harter864@gmail.com>
2022-11-01 18:58:54 +0000

    Changing testing script to pyspedas

--- pyqtgraphtestertemp.py
+++ pyqtgraphtestertemp.py
@@ -1,30 +1,5 @@
-from PyQt5 import QtWidgets
-from pyqtgraph import PlotWidget, plot
-import pyqtgraph as pg
-import sys  # We need sys so that we can pass argv to QApplication
-import os
+import pyspedas
+pyspedas.mms.fgm()
 
-class MainWindow(QtWidgets.QMainWindow):
-
-    def __init__(self, *args, **kwargs):
-        super(MainWindow, self).__init__(*args, **kwargs)
-
-        self.graphWidget = pg.PlotWidget()
-        self.setCentralWidget(self.graphWidget)
-
-        hour = [1,2,3,4,5,6,7,8,9,10]
-        temperature = [30,32,34,32,33,31,29,32,35,45]
-
-        # plot data: x, y values
-        self.graphWidget.plot(hour, temperature)
-
-
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    main = MainWindow()
-    main.show()
-    sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
+from pytplot import tplot
+tplot(['mms1_fgm_b_gse_srvy_l2_btot', 'mms1_fgm_b_gse_srvy_l2_bvec'])
