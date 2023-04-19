56291045664d8555633acf2896224c0b15c94036
supervised <egrimes@igpp.ucla.edu>
2023-04-19 11:39:45 -0700

    adding a test suite for tplot_restore functionality

---
+++ tests/test_tplot_restore.py
@@ -0,0 +1,37 @@
+import os
+from pytplot import tplot_restore, tplot, get
+
+current_directory = os.path.dirname(os.path.realpath(__file__))
+
+current_directory = '/Users/eric/PyTplot2/tests'
+
+
+def test_basic_line():
+    tplot_restore(os.path.join(current_directory, 'testfiles', 'mms1_dis_numberdensity_fast.tplot'))
+    tplot('mms1_dis_numberdensity_fast', display=False, save_png='restore_basic_line')
+
+
+def test_spec():
+    tplot_restore(os.path.join(current_directory, 'testfiles', 'mms1_dis_energyspectr_omni_fast.tplot'))
+    tplot('mms1_dis_energyspectr_omni_fast', display=False, save_png='restore_spec')
+
+
+def test_vec():
+    tplot_restore(os.path.join(current_directory, 'testfiles', 'mms1_dis_bulkv_gse_fast.tplot'))
+    tplot('mms1_dis_bulkv_gse_fast', display=False, save_png='restore_vec')
+
+
+def test_tensor():
+    tplot_restore(os.path.join(current_directory, 'testfiles', 'mms1_dis_prestensor_gse_fast.tplot'))
+
+
+def test_dist():
+    tplot_restore(os.path.join(current_directory, 'testfiles', 'mms1_dis_dist_fast.tplot'))
+
+
+def test_data_att():
+    tplot_restore(os.path.join(current_directory, 'testfiles', 'data_att_test.tplot'))
+    m = get('tha_state_pos', metadata=True)
+    assert 'data_att' in m
+    assert 'coord_sys' in m['data_att']
+    assert 'units' in m['data_att']
